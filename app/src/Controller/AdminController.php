a505657a6e0f3fa5e349a2973c14b605d2d4eb18
damienn7 <damiennicolleau7@gmail.com>
2026-06-24 15:37:21 +0200

    feat(adminRedirect): add admin redirection on /admin to /admin/events endpoint

---
+++ app/src/Controller/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+final class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'admin_dashboard')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function index(): Response
+    {
+        return $this->redirectToRoute('admin_event_index');
+    }
+}
